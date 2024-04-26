f492cd49eda17e52496adc29302b03ba94b6d8e2
51444218+gavrprog@users.noreply.github.com <gavr.prog@gmail.com>
2024-04-26 16:08:09 +0200

    create SetChamfer

--- src/components/setChamfer.js
+++ src/components/setChamfer.js
@@ -3,24 +3,25 @@ import axios from 'axios'
 import "../css/setChamfer.css"
 
 function Chamfer({arrayChamfer, handleClick, currentChamfer}) {
-    
+    console.log(arrayChamfer)
     return (
         arrayChamfer
         .filter((chamfer) => chamfer.view !== "")
         .map((chamfer) => (
-                    <div key={chamfer.process}>
-                        <label htmlFor={chamfer.process}>
-                            <img src={'http://interkam.od.ua/calculator/img/chamfers/' + chamfer.view} alt={chamfer.process}/>
-                        </label>
-                        <input 
-                            id={chamfer.process}
-                            type="radio"
-                            name="chamfer"
-                            value={chamfer.process}
-                            checked={currentChamfer === chamfer.process}
-                            onChange={handleClick} />
-                    </div>
-                )
+                <div key={chamfer.process}>
+                    <label htmlFor={chamfer.process}>
+                        <img src={'http://interkam.od.ua/calculator/img/chamfers/' + chamfer.view} alt={chamfer.process}/>
+                    </label>
+                    <input 
+                        id={chamfer.process}
+                        type="radio"
+                        name="chamfer"
+                        value={chamfer.process}
+                        checked={currentChamfer === chamfer.process}
+                        onChange={handleClick} 
+                    />
+                </div>
+            )
 
         )
     )
@@ -48,36 +49,7 @@ export default function SetChamfer() {
                 <h2><span className="num-step-title">4</span>Выбор фаски:</h2>
             </div>
             <div className="edges">
-
                 <Chamfer arrayChamfer={arrayChamfer} handleClick={handleClick} currentChamfer={currentChamfer}/>
-                {/* <div>
-                    <label htmlFor="chamfer-E"><img src="../img/edge1.jpg" alt=""/></label>
-                    <input id="chamfer-E" type="radio" name="chamfer" className="data-for-calculation" checked value="chamferE"/>
-                </div>
-                <div>
-                    <label htmlFor="chamfer-corner"><img src="../img/edge2.jpg" alt=""/></label>
-                    <input id="chamfer-corner" type="radio" name="chamfer" className="data-for-calculation" value="chamferCorner"/>
-                </div>
-                <div>
-                    <label htmlFor="chamfer-A"><img src="../img/edge3.jpg" alt=""/></label>
-                    <input id="chamfer-A" type="radio" name="chamfer" className="data-for-calculation" value="chamferA"/>
-                </div>
-                <div>
-                    <label htmlFor="chamfer-V"><img src="../img/edge4.jpg" alt=""/></label>
-                    <input id="chamfer-V" type="radio" name="chamfer" className="data-for-calculation" value="chamferV"/>
-                </div>
-                <div>
-                    <label htmlFor="chamfer-H"><img src="../img/edge5.jpg" alt=""/></label>
-                    <input id="chamfer-H" type="radio" name="chamfer" className="data-for-calculation" value="chamferH"/>
-                </div>
-                <div>
-                    <label htmlFor="chamfer-O"><img src="../img/edge5.jpg" alt=""/></label>
-                    <input id="chamfer-O" type="radio" name="chamfer" className="data-for-calculation" value="chamferO"/>
-                </div>
-                <div>
-                    <label htmlFor="chamfer-duck"><img src="../img/edge5.jpg" alt=""/></label>
-                    <input id="chamfer-duck" type="radio" name="chamfer" className="data-for-calculation" value="сhamferDuck"/>
-                </div> */}
             </div>
         </>
     )
